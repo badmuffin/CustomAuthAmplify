3900f571e139489d6461531293833b453a13915d
ashutosh <ashutosh.g@magureinc.com>
2025-01-17 17:47:02 +0530

    user session context has been created

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,18 @@
+import { SignInOutput } from "@aws-amplify/auth";
 export interface IAwsApiResponse<T> {
   success: boolean;
   errorMsg?: string | null;
   response?: T | null;
+}
+
+export interface IUserData {
+  userId: string;
+  username: string;
+  email: string;
+  signInDetails: SignInOutput;
+}
+
+export interface ICogUser {
+  cogId: string;
+  email: string;
 }
